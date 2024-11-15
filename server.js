063de347ca24cfc4356c7bc259fa11350f011775
msouon <jackyl662570078@gmail.com>
2024-11-15 11:27:28 +0800

    2024

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ var server = express();
 //web root
 server.use(express.static(__dirname+"/AgencyProject"));
 
+Services.get("/services",(req,res)->{
+var Services=[
+    {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+    {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
+
+    ];
+    res.send(Services);
+});
+
+
+server.get("/profolio", (req,res)=>{       
+//DB        
+res.send("Profolio");  })
+
 server.listen(80, ()=>{
     console.log("Server is running at port 80.");
 })
